9043b87c33063aeeb70099f1ff480ce7d53fc9f5
vishwajeet-13 <vishwajeetthakur403@gmail.com>
2025-06-22 22:55:20 +0530

    first commit

---
+++ inv/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/',include('invoiceapp.urls')),
+    #simple-jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
