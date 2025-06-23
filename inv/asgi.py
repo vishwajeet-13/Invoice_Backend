4e30ce161701e3d7d39c149a2197c647c1bad7ca
vishwajeet-13 <vishwajeetthakur403@gmail.com>
2025-06-23 11:17:56 +0530

    minor changes

--- inv/asgi.py
+++ inv/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'inv.deployement_settings' if 'RENDER_EXTRENAL_HOSTNAME' in os.environ else 'inv.settings'
+settings_module = 'inv.deployement_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'inv.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
